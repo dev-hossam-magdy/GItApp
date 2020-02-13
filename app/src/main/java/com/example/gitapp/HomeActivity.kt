787a06100edcac266a2b873eb4d9ffafb806019f
hossam <hossamkotp89@gmail.com>
2020-02-13 14:04:15 +0200

    create home activity

---
+++ app/src/main/java/com/example/gitapp/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.gitapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        var handler = Handler()
+
+        handler.postDelayed({},200)
+    }
+}
